9272b95d1d98db62629df57e905a9fe297eadfe4
ahmedmt3 <85899361+ahmedmt3@users.noreply.github.com>
2024-08-12 14:38:55 +0300

    API requests updated

--- lib/util/services/api_services.dart
+++ lib/util/services/api_services.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:note_app/core/config/app_config.dart';
-import 'package:path/path.dart';
 
 class ApiServices {
   Map<String, String> myheaders = {
     'authorization': "Basic ${base64Encode(utf8.encode('ahmed:amt123'))}"
   };
 
+  //==========================[ GET ]============================
   getRequest({required String endPoint}) async {
     String fullUrl = AppConfig.baseUrl + endPoint;
 
@@ -23,46 +22,71 @@ class ApiServices {
     }
   }
 
-  //=======================================================
+  //==========================[ POST ]============================
   postRequest({required String endPoint, data}) async {
     String fullUrl = AppConfig.baseUrl + endPoint;
 
     http.Response response =
         await http.post(Uri.parse(fullUrl), body: data, headers: myheaders);
 
-    if (response.statusCode == 200) {
+    if (response.statusCode == 201) {
       return jsonDecode(response.body);
     } else {
       return null;
     }
   }
 
-  //=======================================================
-  postRequestWithFile({
-    required String endPoint,
-    required String fileField,
-    File? file,
-    required Map data,
-  }) async {
+  //==========================[ PATCH ]============================
+  patchRequest({required String endPoint, data}) async {
     String fullUrl = AppConfig.baseUrl + endPoint;
-    var request = http.MultipartRequest('POST', Uri.parse(fullUrl));
-    request.headers.addAll(myheaders);
 
-    if (file != null) {
-      int length = await file.length();
-      var stream = http.ByteStream(file.openRead());
-      var multipartfile = http.MultipartFile(fileField, stream, length,
-          filename: basename(file.path));
-      request.files.add(multipartfile);
-    }
+    http.Response response = await http.patch(Uri.parse(fullUrl), body: data);
 
-    data.forEach((key, value) => request.fields[key] = value);
-    var streamedResponse = await request.send();
-    var response = await http.Response.fromStream(streamedResponse);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
       return null;
     }
   }
+
+  //==========================[ DELETE ]============================
+  deleteRequest({required String endPoint, data}) async{
+    String fullUrl = AppConfig.baseUrl + endPoint;
+
+    http.Response response = await http.patch(Uri.parse(fullUrl), body: data);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+  //=======================================================
+  // postRequestWithFile({
+  //   required String endPoint,
+  //   required String fileField,
+  //   File? file,
+  //   required Map data,
+  // }) async {
+  //   String fullUrl = AppConfig.baseUrl + endPoint;
+  //   var request = http.MultipartRequest('POST', Uri.parse(fullUrl));
+  //   request.headers.addAll(myheaders);
+
+  //   if (file != null) {
+  //     int length = await file.length();
+  //     var stream = http.ByteStream(file.openRead());
+  //     var multipartfile = http.MultipartFile(fileField, stream, length,
+  //         filename: basename(file.path));
+  //     request.files.add(multipartfile);
+  //   }
+
+  //   data.forEach((key, value) => request.fields[key] = value);
+  //   var streamedResponse = await request.send();
+  //   var response = await http.Response.fromStream(streamedResponse);
+  //   if (response.statusCode == 200) {
+  //     return jsonDecode(response.body);
+  //   } else {
+  //     return null;
+  //   }
+  // }
 }
